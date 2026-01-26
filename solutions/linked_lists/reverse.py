96cf503df6cc82e24f63fe513238736687b30474
John Washam <code@readyprompt.com>
2026-01-26 12:26:01 -0800

    Linked lists

---
+++ solutions/linked_lists/reverse.py
@@ -0,0 +1,25 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        next = None
+        last = None
+        curr = head
+
+        while curr is not None:
+            next = curr.next
+            curr.next = last
+            last = curr
+            curr = next
+
+        return last
+
+#                          c
+#                          n
+#                     l
+
+    # n       n       n
